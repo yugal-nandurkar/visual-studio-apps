24a1a8ff9f7c9b1ff9aebc88a2f48bcc8099a4a7
yugal-nandurkar <yugalnandurkar5@gmail.com>
2024-10-26 01:27:37 +0530

    Razor Commit 3

---
+++ RazorPagesMovie/RazorPagesMovie/Migrations/20241025194909_New_DataAnnotations.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RazorPagesMovie.Migrations
+{
+    /// <inheritdoc />
+    public partial class New_DataAnnotations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
